0156bf7da2ec9e858f92716a025b8c975e6ede80
Georg Schuppe <georg.schuppe@gmail.com>
2021-02-23 14:02:00 +0100

    (test) added prism IO test

--- test/test_prism_io.py
+++ test/test_prism_io.py
@@ -1,8 +1,22 @@
 import unittest
+import os
 import networkx as nx
 from prismhandler.prism_io import write_prism_model
 
 
 class PrismIOTest(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        self.test_game = nx.DiGraph()
+        self.test_game.graph['init'] = '1'
+        self.test_game.graph['acc'] = ['1']
+        self.test_game.add_node('1', player=1)
+        self.test_game.add_node('2', player=2)
+        self.test_game.add_node('3', player=0)
+        self.test_game.add_edge('1', '2')
+        self.test_game.add_edge('2', '3')
+        self.test_game.add_edge('3', '1', prob=1)
+
+    def test_write_prism_model(self):
+        write_prism_model(self.test_game, 'test')
+        self.assertTrue(os.path.exists('data/test.prism'))
