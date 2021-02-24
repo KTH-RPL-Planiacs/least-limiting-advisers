f6dc0d9b677816132b2625cfb7e25257e617d1ee
Georg Schuppe <georg.schuppe@gmail.com>
2021-02-24 10:11:13 +0100

    (test) updated prism IO test case to better reflect the new requirements

--- test/test_prism_io.py
+++ test/test_prism_io.py
@@ -11,11 +11,13 @@ class PrismIOTest(unittest.TestCase):
         self.test_game.graph['init'] = '1'
         self.test_game.graph['acc'] = ['1']
         self.test_game.add_node('1', player=1)
+        self.test_game.add_node('1.5', player=0)
         self.test_game.add_node('2', player=2)
         self.test_game.add_node('3', player=0)
-        self.test_game.add_edge('1', '2')
+        self.test_game.add_edge('1', '1.5')
         self.test_game.add_edge('2', '3')
-        self.test_game.add_edge('3', '1', prob=1)
+        self.test_game.add_edge('3', '1', prob=1.0)
+        self.test_game.add_edge('1.5', '2', prob=1.0)
 
     def test_write_prism_model(self):
         write_prism_model(self.test_game, 'test')
