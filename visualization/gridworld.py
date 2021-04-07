06b80fd60bcccbd4d2e6ee7ee0f60a7b3a368cf0
Georg Schuppe <schuppe@kth.se>
2021-04-07 17:17:56 +0200

    feat: started simulation for visualization

--- visualization/gridworld.py
+++ visualization/gridworld.py
@@ -1,17 +1,30 @@
 import pygame
 import time
 import pickle
+import random
+
+
+class RobotView(pygame.sprite.Sprite):
+
+    def __init__(self, name, robot_id, width, height):
+        super().__init__()
+        self.robot_id = robot_id
+        self.image = pygame.image.load('data/robot%i.png' % (robot_id+1)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (int(width), int(height)))
+        self.rect = self.image.get_rect()
+        # self.image.set_colorkey((255, 255, 255))
+        self.name = name
 
 
 class GridWorld:
 
-    def __init__(self, grid, screen_x=500, screen_y=500, cell_margin=5):
+    def __init__(self, grid, robots, screen_x=500, screen_y=500, cell_margin=5):
 
         # define colors
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
         self.GREEN = (0, 255, 0)
-        self.RED = (255, 0, 0)
+        self.RED = (255, 150, 150)
         self.BLUE = (0, 0, 255)
         self.YELLOW = (255, 255, 0)
 
@@ -27,7 +40,7 @@ class GridWorld:
 
         # simulation speed
         self.FPS = 60       # frames per second
-        self.SPEED = 10     # frames per move
+        self.SPEED = 60     # frames per move
         self.frame_count = 0
 
         pygame.init()
@@ -39,6 +52,15 @@ class GridWorld:
 
         self.font = pygame.font.SysFont('arial', 20)
         pygame.display.set_caption("Grid world")
+
+        # agents
+        self.robots = robots
+        self.robot_views = []
+        self.sprites_list = pygame.sprite.Group()
+        for robot_id, robot in enumerate(self.robots):
+            robot_view = RobotView(robot.name, robot_id, self.WIDTH * 0.5, self.HEIGHT * 0.5)
+            self.robot_views.append(robot_view)
+            self.sprites_list.add(robot_view)
 
     def text_objects(self, text, font):
         text_surface = font.render(text, True, self.BLACK)
@@ -67,34 +89,74 @@ class GridWorld:
         # draw the grid
         for row in range(len(self.grid)):
             for col in range(len(self.grid[0])):
-                if self.grid[row][col] == 1:
+                if self.grid[row][col] == 0:
                     self.color = self.BLACK
-                elif self.grid[row][col] == 2:
-                    self.color = self.BLUE
-                else:
+                elif self.grid[row][col] == 1:
                     self.color = self.WHITE
+                elif self.grid[row][col] == 2:
+                    self.color = self.RED
+                else:
+                    self.color = self.BLUE
+
                 pygame.draw.rect(self.screen,
                                  self.color,
                                  [(self.MARGIN + self.WIDTH) * col + self.MARGIN,
                                   (self.MARGIN + self.HEIGHT) * row + self.MARGIN,
                                   self.WIDTH,
                                   self.HEIGHT])
+
+        # display the robots
+        self.sprites_list.draw(self.screen)
+
         # flip the renderer buffer
         pygame.display.flip()
 
     def idle(self, idle_time):
         pass
 
-    def run_frame(self):
+    def run_step(self, states_dict):
         self.frame_count += 1
         if self.frame_count >= self.SPEED:
             self.frame_count = 0
-            if self.cell_count < len(self.grid):
-                self.grid[self.cell_count][self.cell_count] = 2
-            self.cell_count += 1
+            self.simulate_step()
+
+        # update robot views
+        for robot_id, robot in enumerate(self.robots):
+            robot_view = self.robot_views[robot_id]
+            current_coords = states_dict[robot.current_state[0]]
+            displace_x = ((self.WIDTH + self.MARGIN)/2) - (robot_view.rect.width / 2)
+            displace_y = ((self.HEIGHT + self.MARGIN) / 2) - (robot_view.rect.height / 2)
+            robot_view.rect.x = current_coords[1] * (self.WIDTH + self.MARGIN) + displace_x
+            robot_view.rect.y = current_coords[0] * (self.HEIGHT + self.MARGIN) + displace_y
+
+        # render the results
         self.render()
 
-    def loop(self):
+    def simulate_step(self):
+        next_obs = ''
+        next_ap = []
+        for robot in self.robots:
+            # start in a player-1 state
+            mpd_state = robot.current_state[0]
+            action = robot.strategy[robot.current_state]
+            prob_state = None
+            for succ in robot.mdp.successors(mpd_state):
+                if robot.mdp.edges[mpd_state, succ]['act'] == action:
+                    prob_state = succ
+
+            # compute probabilistic outcome of mdp action
+            outcomes = []
+            probs = []
+            for succ in robot.mdp.successors(prob_state):
+                outcomes.append(succ)
+                probs.append(robot.mdp.edges[prob_state, succ]['prob'])
+            choice = random.choices(outcomes, probs)[0]
+            next_obs += robot.mdp.nodes[choice]['ap'][0]
+            next_ap.extend(robot.mdp.graph['ap'])
+
+        # now we have player-2 choices
+
+    def simulate_agents(self, states_dict):
         next_time = time.time()
 
         running = True
@@ -110,17 +172,31 @@ class GridWorld:
             now_time = time.time()
             self.idle(max(0., next_time - now_time))
             if now_time >= next_time:
-                self.run_frame()
+                self.run_step(states_dict)
                 next_time = now_time + (1 / self.FPS)
 
 
 if __name__ == '__main__':
-    agents = pickle.load(open('../data/agents_converged_results.p', 'rb'))
+    pickled_agents = pickle.load(open('data/agents_converged_results.p', 'rb'))
+
+    # mdp player-1-states to coords
+    mdp_states_dict = {'end_top':               (2, 0),
+                       'corridor_top':          (2, 1),
+                       'corridor_top_no_turn':  (2, 1),
+                       'crit':                  (2, 2),
+                       'corridor_bot':          (2, 3),
+                       'corridor_bot_no_turn':  (2, 3),
+                       'end_bot':               (2, 4)}
 
     # build grid structure
-    ex_grid = [[1 for col in range(5)] for row in range(5)]
+    ex_grid = [[0 for col in range(5)] for row in range(5)]
     for i in range(5):
-        ex_grid[2][i] = 0
+        ex_grid[2][i] = 1
+    ex_grid[2][2] = 2
 
-    gridworld = GridWorld(grid=ex_grid, screen_x=500, screen_y=500)
-    gridworld.loop()
+    # set current state of agents
+    for agent in pickled_agents:
+        agent.current_state = agent.synth.graph['init']
+
+    gridworld = GridWorld(grid=ex_grid, robots=pickled_agents, screen_x=500, screen_y=500)
+    gridworld.simulate_agents(mdp_states_dict)
