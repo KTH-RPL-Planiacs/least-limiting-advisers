8c382a1e277bb8359dc40b197cb6db7459504c9e
Georg Schuppe <georg.schuppe@gmail.com>
2021-05-14 15:05:06 +0200

    chore: removed old examples from main

--- main.py
+++ main.py
@@ -14,36 +14,6 @@ def running_example():
     framework.complete_strategy_synthesis('results/running_example.p', verbose=True)
 
 
-def intersection():
-    agents = [AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='A', init_state='end_top'),
-                             formula='F(eba) & G!(crita & critb | crita & critc | crita & critd)'),
-              AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='B', init_state='end_bot'),
-                             formula='F(etb) & G!(critb & crita | critb & critc | critb & critd)'),
-              AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='C', init_state='end_left'),
-                             formula='F(erc) & G!(critc & crita | critc & critb | critc & critd)'),
-              AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='D', init_state='end_right'),
-                             formula='F(eld) & G!(critd & crita | critd & critb | critd & critc)')]
-    framework = AdviserFramework(agents)
-    framework.complete_strategy_synthesis('results/intersection.p', verbose=True)
-
-
-def old_office_example():
-    agents = [AgentSynthGame(mdp=office_clean_mdp(r_id='A', init_state='4,0'),
-                             formula='F(officetra & clean & !officetrc) & '
-                                     'F(officetla & clean & !officetlb)'),
-              AgentSynthGame(mdp=office_clean_mdp(r_id='D', init_state='5,0'),
-                             formula='F(officebrd & clean & !officebrc) & '
-                                     'F(officebld & clean & !officeblb)'),
-              AgentSynthGame(mdp=office_bins_mdp(r_id='B', init_state='5,9'),
-                             formula='F(officeblb & bin) & '
-                                     'F(officetlb & bin)'),
-              AgentSynthGame(mdp=office_bins_mdp(r_id='C', init_state='4,9'),
-                             formula='F(officebrc & bin) & '
-                                     'F(officetrc & bin)')]
-    framework = AdviserFramework(agents)
-    framework.complete_strategy_synthesis('results/old_office_example.p', verbose=True)
-
-
 def office_10x5(n_agents, n_bins):
     agents = []
     for n in range(n_agents):
@@ -68,5 +38,34 @@ def dumb_stuff():
 
 if __name__ == '__main__':
     # running_example()
-    # office_10x5(n_agents=2, n_bins=2)
-    dumb_stuff()
+    office_10x5(n_agents=2, n_bins=2)
+    # dumb_stuff()
+
+# def intersection():
+#     agents = [AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='A', init_state='end_top'),
+#                              formula='F(eba) & G!(crita & critb | crita & critc | crita & critd)'),
+#               AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='B', init_state='end_bot'),
+#                              formula='F(etb) & G!(critb & crita | critb & critc | critb & critd)'),
+#               AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='C', init_state='end_left'),
+#                              formula='F(erc) & G!(critc & crita | critc & critb | critc & critd)'),
+#               AgentSynthGame(mdp=intersection_no_turn_symmetric_labels_mdp(r_id='D', init_state='end_right'),
+#                              formula='F(eld) & G!(critd & crita | critd & critb | critd & critc)')]
+#     framework = AdviserFramework(agents)
+#     framework.complete_strategy_synthesis('results/intersection.p', verbose=True)
+#
+#
+# def old_office_example():
+#     agents = [AgentSynthGame(mdp=office_clean_mdp(r_id='A', init_state='4,0'),
+#                              formula='F(officetra & clean & !officetrc) & '
+#                                      'F(officetla & clean & !officetlb)'),
+#               AgentSynthGame(mdp=office_clean_mdp(r_id='D', init_state='5,0'),
+#                              formula='F(officebrd & clean & !officebrc) & '
+#                                      'F(officebld & clean & !officeblb)'),
+#               AgentSynthGame(mdp=office_bins_mdp(r_id='B', init_state='5,9'),
+#                              formula='F(officeblb & bin) & '
+#                                      'F(officetlb & bin)'),
+#               AgentSynthGame(mdp=office_bins_mdp(r_id='C', init_state='4,9'),
+#                              formula='F(officebrc & bin) & '
+#                                      'F(officetrc & bin)')]
+#     framework = AdviserFramework(agents)
+#     framework.complete_strategy_synthesis('results/old_office_example.p', verbose=True)
