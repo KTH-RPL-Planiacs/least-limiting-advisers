e04e853bc7536ee535c7128b0513f21c2277d436
Georg Schuppe <georg.schuppe@gmail.com>
2021-05-15 21:34:59 +0200

    fix: added additional asserts to guarantee correctness

--- ltlf2dfa_nx.py
+++ ltlf2dfa_nx.py
@@ -3,13 +3,12 @@ from ltlf2dfa.base import MonaProgram
 from ltlf2dfa.ltlf2dfa import invoke_mona, createMonafile
 
 import networkx as nx
-import pygraphviz
 
 import re
 
 
 # Dump a value from a file based on a regex passed in.
-def get_value(text, regex, value_type=float):
+def get_value(text, regex, value_type=str):
     pattern = re.compile(regex, re.MULTILINE)
     results = pattern.search(text)
     if results:
@@ -50,57 +49,47 @@ class LTLf2nxParser:
             print('<LTLf2ndParser.to_dot()> No formula parsed. Please parse a formula first using parse_formula(str)!')
             return None
 
-        dot = self.to_dot()
-        mona = self.to_mona_output()
-
-        # create graph from dot output
-        g = nx.drawing.nx_agraph.from_agraph(pygraphviz.AGraph(dot))
-        # enforce directed graph (instead of MultiDiGraph)
-        g = nx.DiGraph(g)
-        g.graph.clear()
+        mona_output = self.to_mona_output()
+        g = nx.DiGraph()
         g.graph['name'] = name
 
-        # re-label all shape doublecircle nodes as accepting nodes
-        acc = []
-        for node in g.nodes():
-            if 'shape' in g.nodes[node] and g.nodes[node]['shape'] == 'doublecircle':
-                acc.append(node)
-                del g.nodes[node]['shape']
+        # if formula is unsatisfiable
+        if mona_output == "Formula is unsatisfiable":
+            print('Formula is unsatisfiable, DFA not constructed!')
+            return None
 
-        g.graph['acc'] = acc
-
-        # label actual init nodes and remove dummy init node !ASSUMES SINGULAR INIT NODE!
-        init = []
-        for succ in g.successors('init'):
-            init.append(succ)
-
-        g.graph['init'] = init[0]
-        g.remove_node('init')
-
-        # adds alternate edge labels that are easier to parse, but harder to read
-
-        if mona == "Formula is unsatisfiable":
-            # nothing to do
-            return g
-
-        variables = get_value(mona, r'.*DFA for formula with free variables:[\s]*(.*?)\n.*', str)
+        # atomic propositions
+        variables = get_value(mona_output, r'.*DFA for formula with free variables:[\s]*(.*?)\n.*', str)
         g.graph['ap'] = variables.split()
 
-        for line in mona.splitlines():
+        # accepting states
+        accepting_states = get_value(mona_output, r".*Accepting states:[\s]*(.*?)\n.*", str)
+        accepting_states = [
+            str(x.strip()) for x in accepting_states.split() if len(x.strip()) > 0
+        ]
+
+        g.graph['acc'] = accepting_states
+
+        # edges and states
+        for line in mona_output.splitlines():
             if line.startswith("State "):
 
                 orig_state = get_value(line, r".*State[\s]*(\d+):\s.*", str)
                 dest_state = get_value(line, r".*state[\s]*(\d+)[\s]*.*", str)
                 guard = get_value(line, r".*:[\s](.*?)[\s]->.*", str)
-
-                if orig_state == '0':
-                    # already deleted dummy state
-                    continue
-                if 'guard' in g.edges[orig_state, dest_state]:
-                    new_guard = g.edges[orig_state, dest_state]['guard']
-                    new_guard.append(guard)
-                    g.add_edge(orig_state, dest_state, guard=new_guard)
+                if g.has_edge(orig_state, dest_state):
+                    g.edges[orig_state, dest_state]['guard'].append(guard)
                 else:
                     g.add_edge(orig_state, dest_state, guard=[guard])
+
+        # remove the don't care state 0
+        assert g.has_edge('0', '1')
+        assert len(list(g.successors('0'))) == 1
+        assert len(g.edges['0', '1']['guard']) == 1
+        assert all(c == 'X' for c in g.edges['0', '1']['guard'][0])
+        g.remove_node('0')
+
+        # initial state
+        g.graph['init'] = '1'
 
         return g
